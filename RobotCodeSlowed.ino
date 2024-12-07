24b7fe5cf7505814d9ae550b0dbbc9d1afcf5f84
dbarkway <dbarkway@uwo.ca>
2024-12-06 22:51:35 -0500

    Update RobotCodeSlowed.ino

--- RobotCodeSlowed.ino
+++ RobotCodeSlowed.ino
@@ -16,7 +16,7 @@
 #include <ps5Controller.h>
 #include <Arduino.h>
 
-#define PRINT_COLOUR                                  // uncomment to turn on output of colour sensor data
+#define PRINT_COLOUR                                     //uncomment to turn on output of colour sensor data
 
 #include <Adafruit_TCS34725.h>
 #include <Wire.h>
@@ -29,28 +29,28 @@ const int cPWMFreq = 20000;
 
 const int cTCSLED = 23;
 
-const int in1 = 16;     
+const int in1 = 16;                                      //setting the pins of the gear motors     
 const int in2 = 17;     
 const int in3 = 18;     
 const int in4 = 19;   
-const int cServoPin1 = 15;
+const int cServoPin1 = 15;                               //setting the pins for the servo motors
 const int cServoPin2 = 4;
 const int cServoPin3 = 2;
-const long cMinDutyCycle = 1650;      //1650
-const long cMaxDutyCycle = 8175;      //8175
+const long cMinDutyCycle = 1650;                         //the dutycycle of the servo motor to convert it into degrees
+const long cMaxDutyCycle = 8175;      
 
-int servoSpeedDelay = 10;  // Speed control delay value, lower means faster movement
+int servoSpeedDelay = 10;                                //Speed control delay value, lower means faster movement
 int targetServoPos1 = 0;
 int currentServoPos1 = 0;
 
-long degreesToDutyCycle(int deg) {
+long degreesToDutyCycle(int deg) {                       //comparing 0 to 180 degrees to the duty cycle of the servos to have an angle input  
   return map(deg, 0, 180, cMinDutyCycle, cMaxDutyCycle);
 }
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_4X);
-bool tcsFlag = 0;                                     // TCS34725 flag: 1 = connected; 0 = not found
+bool tcsFlag = 0;                                         //TCS34725 flag: 1 = connected; 0 = not found
 
-void onConnect()
+void onConnect()                                          //checking that the controller is connected
 {
   Serial.println("Connected!.\n");
 }
@@ -60,7 +60,7 @@ void onDisConnect()
   Serial.println("Disconnected!.\n");    
 }
 
-void setUpPinModes()
+void setUpPinModes()                                    //setting the motor connections as outputs on the ESP32
 {
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -74,26 +74,26 @@ void setup()
   Serial.begin(115200);
   #endif
   
-  pinMode(cTCSLED, OUTPUT);                           // configure GPIO for control of LED on TCS34725
-  // Connect to TCS34725 colour sensor
+  pinMode(cTCSLED, OUTPUT);                             //configure GPIO for control of LED on TCS34725
+                                                        //Connect to TCS34725 colour sensor
   if (tcs.begin()) {
     Serial.printf("Found TCS34725 colour sensor\n");
     tcsFlag = true;
-    digitalWrite(cTCSLED, 1);                         // turn on onboard LED 
+    digitalWrite(cTCSLED, 1);                           //turn on onboard LED 
   } 
   else {
     Serial.printf("No TCS34725 found ... check your connections\n");
     tcsFlag = false;
   }
 
-  setUpPinModes();
+  setUpPinModes();                                      //calling the void setting the connections as outputs
   ledcAttach(cServoPin1, 50, 16);
   ledcAttach(cServoPin2, 50, 16);
   ledcAttach(cServoPin3, 50, 16);
 
   ps5.attachOnConnect(onConnect);
   ps5.attachOnDisconnect(onDisConnect);
-  ps5.begin("D0:BC:C1:09:BF:B9");           // MAC address of PS5 controller
+  ps5.begin("D0:BC:C1:09:BF:B9");                       //connecting the playstation controller through specific bluetooth mac address
   while (ps5.isConnected() == false) 
   { 
     Serial.println("PS5 controller not found");
@@ -102,7 +102,7 @@ void setup()
   Serial.println("Ready.");
 }
 
-void stationary(){
+void stationary(){                                      //creating each directional movement for the robot by setting the pins high or low
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
@@ -137,8 +137,8 @@ void right() {
   digitalWrite(in4, HIGH);
 }
 
-void pickup() {
-  targetServoPos1 = 100;
+void pickup() {                                         //creating other functions for the motors in the system
+  targetServoPos1 = 100;                                //setting the servo motors to different angles
 }
 
 void droparm() {
@@ -170,8 +170,8 @@ void dropBeads() {
   ledcWrite(cServoPin3, degreesToDutyCycle(servoPos3));
 }
 
-void updateServoPosition() {
-  if (currentServoPos1 < targetServoPos1) {
+void updateServoPosition() {                            //code to control the speed of the servo motor for the arm
+  if (currentServoPos1 < targetServoPos1) {             //makes the servo act like a stepper
     currentServoPos1++;
   } else if (currentServoPos1 > targetServoPos1) {
     currentServoPos1--;
@@ -180,13 +180,13 @@ void updateServoPosition() {
   delay(servoSpeedDelay);  // Adjust speed by changing this value
 }
 
-void loop() {
+void loop() {                                             //continuous process for the system
   uint16_t r, g, b, c;
   
   if (ps5.isConnected() == true){
     while (ps5.isConnected() == true){
 
-      if (ps5.Up()) {
+      if (ps5.Up()) {                                     //connecting buttons on the controller to the functions
         forward();
       }
       if (ps5.Down()) {
@@ -208,8 +208,8 @@ void loop() {
         droparm();
       }
       if (ps5.R2()) {
-        if (tcsFlag) {                                      // if colour sensor initialized
-          tcs.getRawData(&r, &g, &b, &c);                   // get raw RGBC values
+        if (tcsFlag) {                                      //if colour sensor initialized
+          tcs.getRawData(&r, &g, &b, &c);                   //get raw RGBC values
           if (c < 200) {
             green();
           }
@@ -228,7 +228,7 @@ void loop() {
         flat();
         flat2();
       }
-      updateServoPosition(); // Update servo 1 position gradually
+      updateServoPosition();                                 //Update servo 1 position gradually
     }
   }
 }
